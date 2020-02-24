37643df8d5763ca1b657fc0c286729eb2713fbfb
Tamara Undesser <t_und@hotmail.com>
2020-02-24 20:48:42 +0000

    Configure spring-data jpa and rest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.flywaydb:flyway-core")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
