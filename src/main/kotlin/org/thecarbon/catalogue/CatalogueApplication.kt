306f74f7c800dfc1cfc8485505680b400db503ea
Tamara Undesser <t_und@hotmail.com>
2020-02-24 19:29:41 +0000

    Spring Initializer bootstrapped

---
+++ src/main/kotlin/org/thecarbon/catalogue/CatalogueApplication.kt
@@ -0,0 +1,11 @@
+package org.thecarbon.catalogue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogueApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogueApplication>(*args)
+}
---
+++ src/test/kotlin/org/thecarbon/catalogue/CatalogueApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.thecarbon.catalogue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
